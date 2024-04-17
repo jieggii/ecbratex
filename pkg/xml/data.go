9dd269302695e350eaad4be5a94378d7d7a4a78e
jieggii <jieggii@protonmail.com>
2024-04-17 13:00:09 +0300

    Write doc comments in `xml` package

--- pkg/xml/data.go
+++ pkg/xml/data.go
@@ -4,7 +4,7 @@ import (
 	"encoding/xml"
 )
 
-// Data is a struct representing an XML document containing currency exchange rate records.
+// Data is a struct representing an XML document containing currency exchange rates records.
 type Data struct {
 	Cubes []DataCube `xml:"Cube>Cube"`
 }
@@ -19,6 +19,7 @@ type DataCubeRate struct {
 	Rate     float32 `xml:"rate,attr"`
 }
 
+// NewData decodes XML bytes to a new Data.
 func NewData(data []byte) (*Data, error) {
 	xmlData := &Data{}
 	if err := xml.Unmarshal(data, xmlData); err != nil {
