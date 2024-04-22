3ac5248460733caa832d1c8c5441e15dd1811419
jieggii <jieggii@protonmail.com>
2024-04-22 20:44:58 +0300

    Remove String() method from date.Date interface

--- pkg/date/date.go
+++ pkg/date/date.go
@@ -12,8 +12,4 @@ type Date interface {
 
 	// Day returns day of the month.
 	Day() int
-
-	// String returns the string representation of the date in "YYYY-MM-DD" (ISO-8601) format.
-	// For example, "2024-04-24".
-	String() string
 }
